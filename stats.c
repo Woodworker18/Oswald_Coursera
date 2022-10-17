cae9ef950925ed3ad9093860ce87804422c40c95
Doug Oswald <doswald18@gmail.com>
2022-10-17 14:28:44 -0700

    Maximum function roughed out

--- stats.c
+++ stats.c
@@ -35,13 +35,24 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  printf("Hello Hello AGAIN");	
+  printf("Hello Hello AGAIN");
+  printf("\n");	
 
   for (unsigned int i = 0; i < SIZE; ++i)
   {
   	printf("%i\n",test[i]);
   }
 
+  int tmp = test[0];
+  for (unsigned int j = 1; j < SIZE; ++j)
+  {
+    if (test[j]>tmp)
+    {
+    	tmp=test[j];
+        printf("%i\n",test[j]);
+    }  	
+  }
+  printf("Maximum = %i\n",tmp);
 
 
   /* Other Variable Declarations Go Here */
